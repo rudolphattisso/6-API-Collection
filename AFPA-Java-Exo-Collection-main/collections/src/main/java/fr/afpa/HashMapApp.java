d3f4a421556b5eed3998a565dabd39b89aad3f11
Rudolphattisso <attissorudolph@gmail.com>
2024-07-08 14:11:05 +0200

    suprression du fichier target

---
+++ AFPA-Java-Exo-Collection-main/collections/src/main/java/fr/afpa/HashMapApp.java
@@ -0,0 +1,61 @@
+package fr.afpa;
+
+import java.util.HashMap;
+
+//TODO Faites l'import de java HashMap
+
+
+public class HashMapApp {
+    public static void main(String[] args) {
+
+        // Ressources :
+        // https://www.youtube.com/watch?v=I9aBP0xm-lE
+        // https://www.w3schools.com/java/java_hashmap.asp
+        // https://www.jmdoudoux.fr/java/dej/chap-collections.htm#collections-5
+
+
+        // TODO Instanciez une nouvelle HashMap sur le theme de votre choix
+        HashMap<String,String> capitalesCities = new HashMap<String,String>();
+
+        // TODO Ajoutez des paires clé/valeur à votre HashMap - au moins 5 paires
+        capitalesCities.put("Gabon", "Libreville")
+
+        // TODO Récupérez la valeur d'une de vos paires
+        
+
+        // TODO Vérifiez si une de vos clés est contenue dans la collection
+       
+
+        // TODO Vérifiez si une de vos valeurs est contenue dans la collection
+       
+
+        // TODO Ajoutez une paire clé/valeur à votre HashMap
+        
+
+        // TODO Utilisez une méthode pour trouver le nombre de paires (la longueur de votre HashMap) et affichez la en console
+        
+
+        // TODO Affichez toutes les clés de votre HashMap (avec une boucle for each & avec une méthode de HashMap)
+        
+
+        // TODO Affichez toutes les valeurs de votre HashMap (avec une boucle for each & avec une méthode de HashMap)
+       
+
+
+        // TODO Affichez toutes les clés/valeurs de votre HashMap (avec une boucle for each & avec une méthode de HashMap)
+        
+
+        // TODO Supprimez une de vos paires
+       
+
+        // TODO Vérifiez si votre HashMap est vide
+       
+
+        // TODO Supprimez tous les éléments de votre HashMap
+      
+
+        // TODO Refaites la vérification
+    
+
+    }
+}
---
+++ AFPA-Java-Exo-Collection-main/collections/src/main/java/fr/afpa/StackApp.java
@@ -0,0 +1,51 @@
+package fr.afpa;
+
+import java.util.Stack;
+
+public class StackApp {
+    public static void main(String[] args) {
+
+        // Ressources :
+        // https://www.jmdoudoux.fr/java/dej/chap-collections.htm#collections-5 -> crtl + F : '14.6.7.1. L'interface java.util.Deque' (ca aide si vous y retrouvez sinon utilisé les autres)
+        // https://codegym.cc/fr/groups/posts/fr.828.pile-java
+        // https://www.youtube.com/watch?v=KInG04mAjO0
+        
+
+        // TODO Instanciez une nouvelle Stack(Pile) sur le theme de votre choix
+        
+
+        // TODO Ajoutez des valeurs dans votre stack
+            
+
+        // TODO Ajoutez une valeur au début de la stack
+        
+
+        // TODO Affichez votre stack
+        
+
+        // TODO Supprimez l'élément du dessus de votre stack
+        
+        
+        // TODO Affichez l'élément du dessus de votre stack
+        
+
+        // TODO Supprimez le premier élément de votre stack
+        
+
+        // TODO Affichez le premier élément de votre stack
+        
+       
+        // TODO Cherchez et affichez la position d'un de vos éléments (la valeur indiquera la position de l'élément par rapport au dessus de la stack)
+           
+
+        // TODO Vérifiez si votre stack est vide 
+        
+
+        // TODO Supprimez tous les éléments de votre stack
+        
+
+        // TODO Refaites la vérification
+       
+
+    }
+}
